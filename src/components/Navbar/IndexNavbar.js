0107ee018f34b2d5b4f1387e217e431febf55b7a
YA <vector_cp@mail.ru>
2021-03-21 15:40:51 +0300

    feat: add logo, fix: navbar, static page

--- src/components/Navbar/IndexNavbar.js
+++ src/components/Navbar/IndexNavbar.js
@@ -1,60 +1,44 @@
-/*eslint-disable*/
-import React from "react";
 import { Link } from "react-router-dom";
-// components
+import img_logo from "../../assets/img/logo_rslang.png";
+
 
 export default function Navbar(props) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link
-              to="/"
-              className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              <span className = " font-display text-4xl">Rs Lang </span>
+      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow  ">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between ">
+          <div className=" flex justify-between p-2">
+            <Link to="/" className="">
+              <img className="mr-8" src={img_logo} alt="RSLANG" width="300" />
             </Link>
 
-            <Link
-              to="/textbook/"
-              className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              Электронный Учебник
-            </Link>
-
-            <Link
-              to="/games/"
-              className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              Мини-игры
-            </Link>
-
-            <Link
-              to="/settings/"
-              className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              Настройки
-            </Link>
-
-            <Link
-              to="/statistics/"
-              className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              Статистика
-            </Link>
-
-            <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent 
-              rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="fas fa-bars"></i>
-            </button>
+            <div className=" flex justify-between items-center">
+              <Link
+                to="/textbook/"
+                className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-8  whitespace-nowrap uppercase "
+              >
+                Электронный Учебник
+              </Link>
+              <Link
+                to="/games/"
+                className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-8  whitespace-nowrap uppercase"
+              >
+                Мини-игры
+              </Link>
+              <Link
+                to="/settings/"
+                className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-8  whitespace-nowrap uppercase"
+              >
+                Настройки
+              </Link>
+              <Link
+                to="/statistics/"
+                className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-8  whitespace-nowrap uppercase"
+              >
+                Статистика
+              </Link>
+            </div>
           </div>
-      
         </div>
       </nav>
     </>
