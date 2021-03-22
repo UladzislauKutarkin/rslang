4e11d94fb4c2030cfe1cd75de0a3f799e76f2da7
Yauheni Beiduk <yauheni.beiduk@gmail.com>
2021-03-22 21:08:46 +0300

    Feat:add redux store and SignUp component

---
+++ src/redux/auth/user.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+export const FETCH_USER_REQUEST = "FETCH_USER_REQUEST";
+export const FETCH_USER_SUCSESS = "FETCH_USER_SUCSESS";
+export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+
+let initialState = {
+  user: {},
+  isLoading: false,
+  error: '' 
+};
+
+const SignUpReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+      return { ...state,
+         isLoading: true,
+         user: {},
+        error: '' };
+    case FETCH_USER_SUCSESS:
+      return { ...state, 
+        isLoading: false, 
+        user: action.payload,
+         error: '' };
+    case FETCH_USER_FAILURE:
+      return { 
+        ...state, 
+        isLoading: false, 
+        user: {}, 
+        error: action.payload 
+      };
+    default:
+      return state;
+  }
+};
+
+export const createUser = (user) => {
+  return dispatch => {
+    console.log(user);
+    dispatch(fetchUserRequest());
+    axios
+      .post("https://rs-lang-back.herokuapp.com/users", { ...user })
+      .then(({ data }) => dispatch(fetchUserSucsess(data)))
+      .catch((error) => dispatch(fetchUserFailure(error)));
+  };
+};
+
+
+export const fetchUserRequest = () => ({ type: FETCH_USER_REQUEST });
+export const fetchUserSucsess = (data) => ({ type: FETCH_USER_SUCSESS, payload: data });
+export const fetchUserFailure = (error) => ({
+  type: FETCH_USER_FAILURE,
+  payload: error.message,
+});
+export default SignUpReducer;
