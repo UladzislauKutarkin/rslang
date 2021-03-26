aa493f8d97b1601a92111703fa330d7fc19fef62
Yauheni Beiduk <yauheni.beiduk@gmail.com>
2021-03-26 10:08:12 +0300

    Feat:add eslint, prettier and Settings component

---
+++ src/components/pages/TextBookPage/ChangeButtons.jsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux"
+import React, { useCallback, useState } from "react"
+import { changeSettingsButtons } from "../../../redux/settings/settings"
+
+const ChangeButtons = () => {
+  const buttonOn = useSelector(({ settings }) => settings.buttons)
+  const [button, setButton] = useState(buttonOn)
+  const dispatch = useDispatch()
+  const handleSwitchButtons = useCallback(
+    (e) => {
+      dispatch(changeSettingsButtons(e.target.checked))
+      setButton(e.target.checked)
+    },
+    [dispatch]
+  )
+
+  return (
+    <>
+      <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+        <input
+          checked={button}
+          onChange={handleSwitchButtons}
+          type="checkbox"
+          name="toggle"
+          id="toggle"
+          className="toggle-checkbox outline-none	 absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+        />
+        <label
+          htmlFor="toggle"
+          className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+        />
+      </div>
+      <label htmlFor="toggle" className="text-xs text-gray-700">
+        Нажми меня
+      </label>
+    </>
+  )
+}
+
+export default ChangeButtons
---
+++ src/redux/pagination/pagination.jsx
@@ -0,0 +1,29 @@
+export const CHANGE_PAGE = "CHANGE_PAGE"
+export const CHANGE_GROUP = "CHANGE_GROUP"
+
+const initialState = {
+  page: 0,
+  group: 0,
+}
+
+const PaginationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_PAGE:
+      return { ...state, page: action.payload }
+    case CHANGE_GROUP:
+      return { ...state, group: action.payload }
+    default:
+      return state
+  }
+}
+
+export const changePage = (page) => ({
+  type: CHANGE_PAGE,
+  payload: page,
+})
+export const changeGroup = (group) => ({
+  type: CHANGE_GROUP,
+  payload: group,
+})
+
+export default PaginationReducer
