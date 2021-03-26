e623f45514e87279339c701af1a5c1d352e2e3a3
Yauheni Beiduk <yauheni.beiduk@gmail.com>
2021-03-26 19:06:37 +0300

    Fix: delete Settings component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Switch, Route, Redirect } from "react-router-dom"
 import HomePage from "./components/pages/HomePage/HomePage"
 import TextBookPage from "./components/pages/TextBookPage/TextBookPage"
 import GamesPage from "./components/pages/GamesPage/GamesPage"
-import SettingsPage from "./components/pages/SettingsPage/SettingsPage"
 import StatisticsPage from "./components/pages/StatisticsPage/StatisticsPage"
 import SignUp from "./components/Register/Register"
 import SignIn from "./components/SignIn/SignIn"
@@ -16,7 +15,6 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/textbook/" component={TextBookPage} />
         <Route exact path="/games/" component={GamesPage} />
-        <Route exact path="/Settings/" component={SettingsPage} />
         <Route exact path="/statistics/" component={StatisticsPage} />
         <Route exact path="/register/" component={SignUp} />
         <Route exact path="/signin/" component={SignIn} />
