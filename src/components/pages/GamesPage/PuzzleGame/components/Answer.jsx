d72cfb09f2746311f301125058428bf95701bd21
AlekseyGrimm <stalpetc@gmail.com>
2021-03-30 12:18:02 +0300

    feat: update this code and add new components

---
+++ src/components/pages/GamesPage/PuzzleGame/components/Answer.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Container, Form } from 'semantic-ui-react';
+
+class Answer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            value: '',
+        };
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({ value: event.target.value.toUpperCase() });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.value);
+        this.setState({
+            value: '',
+        })
+    }
+
+    render() {
+        if (this.props.gameType === "home") {
+            return null;
+        }
+        return (
+            <Container className="answer-container">
+                <Form onSubmit={(e) => this.handleSubmit(e)}>
+                    <Form.Input placeholder="Answer..." name="answer"
+                        value={this.state.value} onChange={(e) => this.handleChange(e)} />
+                    <Form.Button content="Submit" />
+                </Form>
+            </Container>
+        );
+    }
+}
+
+export default Answer;
