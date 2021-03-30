34f588fb9294961a2d419742f478edbc54c54be8
AlekseyGrimm <stalpetc@gmail.com>
2021-03-30 12:08:23 +0300

    add new component

---
+++ src/components/pages/GamesPage/PuzzleGame/components/Field.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+function Field(props) {
+    const isHighlighted = props.isHighlighted;
+    return (
+        <div className={"difficulty-box " + (isHighlighted ? "highlighted" : "regular")}
+            onClick={() => props.onClick()}>
+            <Header as='h4'>{props.name}</Header>
+            {props.desc}
+        </div>
+    );
+}
+
+export default Field
