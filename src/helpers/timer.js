0b65da3e4a4f2f9ee2d7205b7130d33b6ce51a4c
Yauheni Beiduk <yauheni.beiduk@gmail.com>
2021-03-31 19:27:00 +0300

    Fix:fix conflicts

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -2,12 +2,9 @@
 export const Timer = {
   on_10_seconds: false,
   set_10_seconds() {
-    console.log("timer - ", this.on_10_seconds)
     Timer.on_10_seconds = true
-    console.log("timer - ", this.on_10_seconds)
     setTimeout(() => {
       Timer.on_10_seconds = false
-      console.log("timer - ", this.on_10_seconds)
     }, 3000)
   },
 }
